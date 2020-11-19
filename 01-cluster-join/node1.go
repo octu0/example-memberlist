8509bb81e414b3e80fc5572067f15e904c105bb4
Yusuke Hata <yusuke.hata@mirrativ.co.jp>
2020-11-19 15:57:06 +0900

    go fmt

--- 01-cluster-join/node1.go
+++ 01-cluster-join/node1.go
@@ -1,45 +1,46 @@
 package main
 
-import(
-  "log"
-  "os"
-  "os/signal"
-  "syscall"
-  "gopkg.in/urfave/cli.v1"
-  "github.com/hashicorp/memberlist"
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/hashicorp/memberlist"
+	"gopkg.in/urfave/cli.v1"
 )
 
 func action(c *cli.Context) {
-  conf := memberlist.DefaultLocalConfig()
-  conf.Name = "node1"
+	conf := memberlist.DefaultLocalConfig()
+	conf.Name = "node1"
 
-  list, err := memberlist.Create(conf)
-  if err != nil {
-    log.Fatal(err)
-  }
+	list, err := memberlist.Create(conf)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  local := list.LocalNode()
-  log.Printf("node1 at %s:%d", local.Addr.To4().String(), local.Port)
+	local := list.LocalNode()
+	log.Printf("node1 at %s:%d", local.Addr.To4().String(), local.Port)
 
-  log.Printf("wait for other member connections")
-  wait_signal()
+	log.Printf("wait for other member connections")
+	wait_signal()
 }
 
-func wait_signal(){
-  signal_chan := make(chan os.Signal, 2)
-  signal.Notify(signal_chan, syscall.SIGTERM)
-  signal.Notify(signal_chan, syscall.SIGINT)
-  for {
-    select {
-    case s := <-signal_chan:
-      log.Printf("signal %s happen", s.String())
-      return
-    }
-  }
+func wait_signal() {
+	signal_chan := make(chan os.Signal, 2)
+	signal.Notify(signal_chan, syscall.SIGTERM)
+	signal.Notify(signal_chan, syscall.SIGINT)
+	for {
+		select {
+		case s := <-signal_chan:
+			log.Printf("signal %s happen", s.String())
+			return
+		}
+	}
 }
 
-func main(){
-  app         := cli.NewApp()
-  app.Action   = action
-  app.Run(os.Args)
+func main() {
+	app := cli.NewApp()
+	app.Action = action
+	app.Run(os.Args)
 }
--- 01-cluster-join/node2.go
+++ 01-cluster-join/node2.go
@@ -1,48 +1,49 @@
 package main
 
-import(
-  "log"
-  "os"
-  "gopkg.in/urfave/cli.v1"
-  "github.com/hashicorp/memberlist"
+import (
+	"log"
+	"os"
+
+	"github.com/hashicorp/memberlist"
+	"gopkg.in/urfave/cli.v1"
 )
 
 func action(c *cli.Context) {
-  conf := memberlist.DefaultLocalConfig()
-  conf.Name = "node2"
-  conf.BindPort = 7947 // avoid port confliction
-  conf.AdvertisePort = conf.BindPort
+	conf := memberlist.DefaultLocalConfig()
+	conf.Name = "node2"
+	conf.BindPort = 7947 // avoid port confliction
+	conf.AdvertisePort = conf.BindPort
 
-  list, err := memberlist.Create(conf)
-  if err != nil {
-    log.Fatal(err)
-  }
+	list, err := memberlist.Create(conf)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  local := list.LocalNode()
-  log.Printf("node2 at %s:%d", local.Addr.To4().String(), local.Port)
+	local := list.LocalNode()
+	log.Printf("node2 at %s:%d", local.Addr.To4().String(), local.Port)
 
-  join := c.String("join")
-  log.Printf("cluster join to %s", join)
+	join := c.String("join")
+	log.Printf("cluster join to %s", join)
 
-  if _, err := list.Join([]string{join}); err != nil {
-    log.Fatal(err)
-  }
+	if _, err := list.Join([]string{join}); err != nil {
+		log.Fatal(err)
+	}
 
-  for _, member := range list.Members() {
-    log.Printf("Member: %s(%s:%d)", member.Name, member.Addr.To4().String(), member.Port)
-  }
+	for _, member := range list.Members() {
+		log.Printf("Member: %s(%s:%d)", member.Name, member.Addr.To4().String(), member.Port)
+	}
 }
 
-func main(){
-  app         := cli.NewApp()
-  app.Action   = action
-  app.Flags    = []cli.Flag{
-    cli.StringFlag{
-      Name: "join, j",
-      Usage: "cluster join address",
-      Value: "127.0.0.1:xxxx",
-      EnvVar: "JOIN_ADDR",
-    },
-  }
-  app.Run(os.Args)
+func main() {
+	app := cli.NewApp()
+	app.Action = action
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:   "join, j",
+			Usage:  "cluster join address",
+			Value:  "127.0.0.1:xxxx",
+			EnvVar: "JOIN_ADDR",
+		},
+	}
+	app.Run(os.Args)
 }
