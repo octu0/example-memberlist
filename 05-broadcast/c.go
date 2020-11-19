0e4e142d38f89b9e9e8a3e13dd009d65ba80aba9
Yusuke Hata <yusuke.hata@mirrativ.co.jp>
2020-11-19 16:01:14 +0900

    go fmt

--- 05-broadcast/c.go
+++ 05-broadcast/c.go
@@ -1,84 +1,88 @@
 package main
 
-import(
-  "log"
-  "os"
-  "os/signal"
-  "syscall"
-  "context"
-  "encoding/json"
-  "github.com/hashicorp/memberlist"
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/hashicorp/memberlist"
 )
 
 type MyDelegate struct {
-  msgCh      chan []byte
-  broadcasts *memberlist.TransmitLimitedQueue
+	msgCh      chan []byte
+	broadcasts *memberlist.TransmitLimitedQueue
 }
+
 func (d *MyDelegate) NotifyMsg(msg []byte) {
-  d.msgCh <- msg
+	d.msgCh <- msg
 }
 func (d *MyDelegate) GetBroadcasts(overhead, limit int) [][]byte {
-  return d.broadcasts.GetBroadcasts(overhead, limit)
+	return d.broadcasts.GetBroadcasts(overhead, limit)
 }
 func (d *MyDelegate) NodeMeta(limit int) []byte {
-  // not use, noop
-  return []byte("")
+	// not use, noop
+	return []byte("")
 }
 func (d *MyDelegate) LocalState(join bool) []byte {
-  // not use, noop
-  return []byte("")
+	// not use, noop
+	return []byte("")
 }
 func (d *MyDelegate) MergeRemoteState(buf []byte, join bool) {
-  // not use
+	// not use
 }
 
 type MyEventDelegate struct {
-  Num int
+	Num int
 }
+
 func (d *MyEventDelegate) NotifyJoin(node *memberlist.Node) {
-  d.Num += 1
+	d.Num += 1
 }
 func (d *MyEventDelegate) NotifyLeave(node *memberlist.Node) {
-  d.Num -= 1
+	d.Num -= 1
 }
 func (d *MyEventDelegate) NotifyUpdate(node *memberlist.Node) {
-  // skip
+	// skip
 }
 
 type MyBroadcastMessage struct {
-  Key    string  `json:"key"`
-  Value  uint64  `json:"value"`
+	Key   string `json:"key"`
+	Value uint64 `json:"value"`
 }
+
 func (m MyBroadcastMessage) Invalidates(other memberlist.Broadcast) bool {
-  return false
+	return false
 }
 func (m MyBroadcastMessage) Finished() {
-  // nop
+	// nop
 }
 func (m MyBroadcastMessage) Message() []byte {
-  data, err := json.Marshal(m)
-  if err != nil {
-    return []byte("")
-  }
-  return data
+	data, err := json.Marshal(m)
+	if err != nil {
+		return []byte("")
+	}
+	return data
 }
 
 func ParseMyBroadcastMessage(data []byte) (*MyBroadcastMessage, bool) {
-  msg := new(MyBroadcastMessage)
-  if err := json.Unmarshal(data, &msg); err != nil {
-    return nil, false
-  }
-  return msg, true
+	msg := new(MyBroadcastMessage)
+	if err := json.Unmarshal(data, &msg); err != nil {
+		return nil, false
+	}
+	return msg, true
 }
 
-func wait_signal(cancel context.CancelFunc){
-  signal_chan := make(chan os.Signal, 1)
-  signal.Notify(signal_chan, syscall.SIGINT)
-  for {
-    select {
-    case s := <-signal_chan:
-      log.Printf("signal %s happen", s.String())
-      cancel()
-    }
-  }
+func wait_signal(cancel context.CancelFunc) {
+	signal_chan := make(chan os.Signal, 1)
+	signal.Notify(signal_chan, syscall.SIGINT)
+	for {
+		select {
+		case s := <-signal_chan:
+			log.Printf("signal %s happen", s.String())
+			cancel()
+		}
+	}
 }
--- 05-broadcast/node1.go
+++ 05-broadcast/node1.go
@@ -1,60 +1,61 @@
 package main
 
-import(
-  "log"
-  "fmt"
-  "context"
-  "os"
-  "gopkg.in/urfave/cli.v1"
-  "github.com/hashicorp/memberlist"
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/hashicorp/memberlist"
+	"gopkg.in/urfave/cli.v1"
 )
 
 func action(c *cli.Context) {
-  msgCh := make(chan []byte)
+	msgCh := make(chan []byte)
 
-  d := new(MyDelegate)
-  d.msgCh = msgCh
-  d.broadcasts = new(memberlist.TransmitLimitedQueue)
+	d := new(MyDelegate)
+	d.msgCh = msgCh
+	d.broadcasts = new(memberlist.TransmitLimitedQueue)
 
-  conf := memberlist.DefaultLocalConfig()
-  conf.Name          = "node1"
-  conf.BindPort      = 7947 // avoid port confliction
-  conf.AdvertisePort = conf.BindPort
-  conf.Delegate      = d
+	conf := memberlist.DefaultLocalConfig()
+	conf.Name = "node1"
+	conf.BindPort = 7947 // avoid port confliction
+	conf.AdvertisePort = conf.BindPort
+	conf.Delegate = d
 
-  list, err := memberlist.Create(conf)
-  if err != nil {
-    log.Fatal(err)
-  }
+	list, err := memberlist.Create(conf)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  local := list.LocalNode()
-  list.Join([]string{
-    fmt.Sprintf("%s:%d", local.Addr.To4().String(), local.Port),
-  })
+	local := list.LocalNode()
+	list.Join([]string{
+		fmt.Sprintf("%s:%d", local.Addr.To4().String(), local.Port),
+	})
 
-  stopCtx, cancel := context.WithCancel(context.TODO())
-  go wait_signal(cancel)
+	stopCtx, cancel := context.WithCancel(context.TODO())
+	go wait_signal(cancel)
 
-  run  := true
-  for run {
-    select {
-    case data := <-d.msgCh:
-      msg, ok := ParseMyBroadcastMessage(data)
-      if ok != true {
-        continue
-      }
-      log.Printf("received broadcast msg: key=%s value=%d", msg.Key, msg.Value)
+	run := true
+	for run {
+		select {
+		case data := <-d.msgCh:
+			msg, ok := ParseMyBroadcastMessage(data)
+			if ok != true {
+				continue
+			}
+			log.Printf("received broadcast msg: key=%s value=%d", msg.Key, msg.Value)
 
-    case <-stopCtx.Done():
-      log.Printf("stop called")
-      run = false
-    }
-  }
-  log.Printf("bye.")
+		case <-stopCtx.Done():
+			log.Printf("stop called")
+			run = false
+		}
+	}
+	log.Printf("bye.")
 }
 
-func main(){
-  app         := cli.NewApp()
-  app.Action   = action
-  app.Run(os.Args)
+func main() {
+	app := cli.NewApp()
+	app.Action = action
+	app.Run(os.Args)
 }
